db67634499bb3b27f1e33852c8b1237f2e2c0a92
Cristopher Diego Rodriguez Ayala <cristopherrodriguez1510@gmail.com>
2025-10-31 07:21:25 -0400

    Agregar mensaje de interacción

--- Scripts/2d/casa_final.gd
+++ Scripts/2d/casa_final.gd
@@ -17,32 +17,38 @@ extends Node2D
 ]
 
 @export var retratos_dialogo: Array[String] = [
-	"res://Assets/Retratos/eva.png",
-	"res://Assets/Retratos/abuelo.png",
-	"res://Assets/Retratos/chola1.png",
+	"res://Assets/Retratos/hombre_1.png",
+	"res://Assets/Retratos/hombre_serio.png",
+	"res://Assets/Retratos/hombre_serio.png",
 	"res://Assets/Retratos/hombre_1.png",
 	"res://Assets/Retratos/hombre_1.png"
 ]
 
 var jugador_cerca = false
 var dialogo_terminado = false
+@onready var label_interactuar = $Label  # 👈 referencia al texto
 
 func _on_area_interaccion_body_entered(body):
 	if body.name == "Jugador":
 		jugador_cerca = true
+		if label_interactuar:
+			label_interactuar.visible = true  # 👀 mostrar texto
 
 func _on_area_interaccion_body_exited(body):
 	if body.name == "Jugador":
 		jugador_cerca = false
+		if label_interactuar:
+			label_interactuar.visible = false  # ❌ ocultar texto
 
 func _process(delta):
 	if jugador_cerca and Input.is_action_just_pressed("interact") and not dialogo_terminado:
 		print("Iniciando diálogo con la CasaFinal...")
 		get_parent().start_vn_dialog(dialogo_hombre, nombres_dialogo, retratos_dialogo, self)
 		dialogo_terminado = true
+		if label_interactuar:
+			label_interactuar.visible = false  # ocultar cuando empieza el diálogo
 
 # Este lo llamará Pueblo.gd cuando termine el diálogo VN
 func abrir_puerta():
 	print("La puerta de la casa final se ha abierto...")
-	# Aquí podrías cambiar escena:
 	get_tree().change_scene_to_file("res://Scenes/CasaInterior3D.tscn")
